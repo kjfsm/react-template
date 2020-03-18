7d3b2bb4f1f857338572fea0cc96caeb23240b1c
kjfsm <glaygraze@gmail.com>
2020-03-18 21:40:00 +0900

    修正

--- README.md
+++
@@ -1,5 +0,0 @@
-# react-template
-
-参考 https://ics.media/entry/16329/
-
-vscode に eslint 拡張機能をインストールしてください。
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App:React.FC =()=>(
+  <div>
+        Sample
+  </div>
+);
+
+ReactDOM.render(<App />,document.getElementById('app'));
